2d8bfe01e1d7db644e497f700a0c352d0d4f4249
Vishal Kuo <vishalkuo@revw-172-31-218-45.rn.uwaterloo.ca>
2015-03-04 00:13:05 -0500

    Working network connections

--- app/src/main/java/vishalkuo/com/futuresrevealed/MainActivity.java
+++ app/src/main/java/vishalkuo/com/futuresrevealed/MainActivity.java
@@ -2,17 +2,37 @@ package vishalkuo.com.futuresrevealed;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
     private ImageView mainLogo;
     private String url = "http://futuresrevealed.ca";
+
+    private Button signUp;
 
 
     @Override
@@ -29,7 +49,65 @@ public class MainActivity extends ActionBarActivity {
                 startActivity(i);
             }
         });
+
+        signUp = (Button)findViewById(R.id.signUp);
+
+
+
     }
+
+    public void send2(View v){
+        new send().execute();
+    }
+
+    public class send extends AsyncTask<String, String, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            try{
+                // url where the data will be posted
+                String postReceiverUrl = "http://vishalkuo.com/phptest.php";
+                Log.v("postURL: ",  postReceiverUrl);
+
+                // HttpClient
+                HttpClient httpClient = new DefaultHttpClient();
+
+                // post header
+                HttpPost httpPost = new HttpPost(postReceiverUrl);
+
+                // add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                    nameValuePairs.add(new BasicNameValuePair("firstname", "Mike"));
+                nameValuePairs.add(new BasicNameValuePair("lastname", "Dalisay"));
+                nameValuePairs.add(new BasicNameValuePair("email", "mike@testmail.com"));
+
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // execute HTTP post request
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity resEntity = response.getEntity();
+
+                if (resEntity != null) {
+
+                    String responseStr = EntityUtils.toString(resEntity).trim();
+                    Log.v("LOL", "Response: " +  responseStr);
+
+                    // you can add an if statement here and do other actions based on the response
+                }
+
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+            }
+
+
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+    }
+
 
 
     @Override
