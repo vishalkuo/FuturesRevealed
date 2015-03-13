401ccb97bb0b3f21653d9da2ab22c0c8a837ca7b
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-03-12 23:39:12 -0400

    list view setup

--- android/app/src/main/java/vishalkuo/com/futuresrevealed/surveyActivity.java
+++ android/app/src/main/java/vishalkuo/com/futuresrevealed/surveyActivity.java
@@ -4,14 +4,46 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class surveyActivity extends ActionBarActivity {
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
+
+        listView = (ListView)findViewById(R.id.SurveyList);
+
+        String[] values = new String[]{
+                "A", "B", "C", "D", "E"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int itemPosition = i;
+
+                String itemValue = (String)listView.getItemAtPosition(itemPosition);
+
+                Toast.makeText(getApplicationContext(), "Position: " + itemPosition + " List Item: "
+                + itemValue, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
 
 
