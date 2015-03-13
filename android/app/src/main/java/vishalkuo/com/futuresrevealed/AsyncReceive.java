a6bd1262c93a0d165ca5e03454ad7cf55815a6b9
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-03-13 10:08:42 -0400

    setup async receive using post

--- android/app/src/main/java/vishalkuo/com/futuresrevealed/AsyncReceive.java
+++ android/app/src/main/java/vishalkuo/com/futuresrevealed/AsyncReceive.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
@@ -21,11 +26,11 @@ import java.io.InputStreamReader;
  */
 public class AsyncReceive extends AsyncTask<String, String, String> {
 
-
+    private String result = "";
     private Context c;
     private ProgressBar prog;
     private int status = 0;
-
+    private InputStream in;
 
     public AsyncReceive(ProgressBar prog, Context c){
         this.prog = prog;
@@ -50,20 +55,49 @@ public class AsyncReceive extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... strings) {
         try{
-            String getUrl = "http://www.vishalkuo.com/phpGet";
-            Log.v("Attempt Conn: ", getUrl);
+            String postUrl = "http://www.vishalkuo.com/phpGet";
+            Log.v("Attempt Conn: ", postUrl);
 
 
             HttpClient client = new DefaultHttpClient();
-            HttpGet httpGet = new HttpGet(getUrl);
+            HttpGet httpPost = new HttpGet(postUrl);
 
-            HttpResponse response = client.execute(httpGet);
-            BufferedReader in = new BufferedReader(new InputStreamReader
-                    (response.getEntity().getContent()));
+            HttpResponse response = client.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+
+
+            in = entity.getContent();
 
             status = 1;
         }
         catch (IOException e){
+            e.printStackTrace();
+        }
+
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                sb.append(line);
+            }
+            in.close();
+
+            result = sb.toString();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try{
+            JSONArray jarr = new JSONArray(result);
+            for (int i = 0; i < jarr.length(); i++){
+                JSONObject jdata = jarr.getJSONObject(i);
+                Log.i("logging", "name: " + jdata.getString("name") +  "description: " + jdata.getString("description"));
+
+            }
+
+        }catch(JSONException e){
             e.printStackTrace();
         }
 
