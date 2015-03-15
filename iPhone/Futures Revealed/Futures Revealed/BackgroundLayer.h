a7cf2bc93c8e97d420be657992ce470b57e41822
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-03-15 19:07:24 -0400

    reworked iPhone settings

---
+++ iPhone/Futures Revealed/Futures Revealed/BackgroundLayer.h
@@ -0,0 +1,17 @@
+//
+//  BackgroundLayer.h
+//  Futures Revealed
+//
+//  Created by Vishal Kuo on 2015-03-15.
+//  Copyright (c) 2015 Futures Revealed. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface BackgroundLayer : NSObject
+
++(CAGradientLayer*)blueGrad;
++(CAGradientLayer*)whiteGrad;
+
+@end
---
+++ iPhone/Futures Revealed/Futures Revealed/BackgroundLayer.m
@@ -0,0 +1,18 @@
+//
+//  BackgroundLayer.m
+//  Futures Revealed
+//
+//  Created by Vishal Kuo on 2015-03-15.
+//  Copyright (c) 2015 Futures Revealed. All rights reserved.
+//
+
+#import "BackgroundLayer.h"
+
+@implementation BackgroundLayer
+
++(CAGradientLayer*)blueGrad{
+    UIColor 
+    
+}
+
+@end
