4bfbea8583280da7485a123a7100482b477bbede
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-03-25 12:52:20 -0400

    Button animations

--- android/app/src/main/java/vishalkuo/com/futuresrevealed/SplashScreen.java
+++ android/app/src/main/java/vishalkuo/com/futuresrevealed/SplashScreen.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -18,6 +21,10 @@ public class SplashScreen extends Activity {
     private TextView openF;
     private TextView part2;
     private TextView part3;
+    private ImageView bg;
+    private Button survey;
+    private Button eList;
+    private Button learnMore;
     private static int _timeOut = 2000;
 
     @Override
@@ -36,15 +43,30 @@ public class SplashScreen extends Activity {
         part3 = (TextView)findViewById(R.id.pt3);
         part3.setTypeface(thickFont);
 
+        bg = (ImageView)findViewById(R.id.bg);
+        survey = (Button)findViewById(R.id.surveys);
+        survey.setTypeface(thickFont);
+
+        eList = (Button)findViewById(R.id.eList);
+        eList.setTypeface(thickFont);
+        learnMore = (Button)findViewById(R.id.learnMore);
+
         Animation scale = AnimationUtils.loadAnimation(this, R.anim.translate);
         Animation scale2 = AnimationUtils.loadAnimation(this, R.anim.translate2);
         Animation scale3 = AnimationUtils.loadAnimation(this, R.anim.translate3);
-
+        Animation alpha = AnimationUtils.loadAnimation(this, R.anim.fade);
+        Animation btn1 = AnimationUtils.loadAnimation(this, R.anim.btn1);
 
 
         openF.startAnimation(scale);
         part2.startAnimation(scale2);
         part3.startAnimation(scale3);
+        bg.startAnimation(alpha);
+        survey.startAnimation(btn1);
+        eList.startAnimation(btn1);
+        learnMore.startAnimation(btn1);
+
+
         scale.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -69,6 +91,14 @@ public class SplashScreen extends Activity {
             @Override
             public void onAnimationRepeat(Animation animation) {
 
+            }
+        });
+
+        survey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), surveyActivity.class);
+                startActivity(i);
             }
         });
 
