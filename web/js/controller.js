a5f42bff797db21565a7de20856a0800f7169a0f
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-07-25 14:11:20 -0400

    NG-update

--- web/js/controller.js
+++ web/js/controller.js
@@ -2,13 +2,24 @@ var app = angular.module('app', [])
 
 
 app.controller('tableController', ['$scope', '$http', function($scope, $http){
+    $scope.editingData = []
+    
     $http.get("http://www.vishalkuo.com/phpGet.php")
     .success(function(response){
+        for (var i = 0; i < response.length; i++){
+            $scope.editingData[response[i].id] = false
+        }
+        
+        console.log($scope.editingData)
         $scope.names = response
         
     })
     
     $scope.modify = function(tableData){
-        
+        $scope.editingData[tableData.id] = true
+    }
+    
+    $scope.update = function(tableData){
+        $scope.editingData[tableData.id] = false
     }
 }])
