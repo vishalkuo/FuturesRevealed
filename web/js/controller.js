e6b752aac4352ae400dedb484a910b30eb56fec6
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-07-25 12:09:26 -0400

    Table data modelling

--- web/js/controller.js
+++ web/js/controller.js
@@ -1,8 +1,14 @@
 var app = angular.module('app', [])
 
+
 app.controller('tableController', ['$scope', '$http', function($scope, $http){
     $http.get("http://www.vishalkuo.com/phpGet.php")
     .success(function(response){
         $scope.names = response
+        
     })
+    
+    $scope.modify = function(tableData){
+        
+    }
 }])
