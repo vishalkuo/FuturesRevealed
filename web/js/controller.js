e1f5eeeef765b24c971242312f1ff89677e79cc3
Vishal Kuo <vfkuo@uwaterloo.ca>
2015-07-25 22:21:37 -0400

    Working delete

--- web/js/controller.js
+++ web/js/controller.js
@@ -3,14 +3,13 @@ var app = angular.module('app', [])
 
 app.controller('tableController', ['$scope', '$http', function($scope, $http){
     $scope.editingData = []
-    
+    maxID = 0
     $http.get("http://www.vishalkuo.com/phpGet.php")
     .success(function(response){
         for (var i = 0; i < response.length; i++){
             $scope.editingData[response[i].id] = false
+            maxID = (response[i].id > maxID)? response[i].id : maxID
         }
-        
-        console.log($scope.editingData)
         $scope.names = response
         
     })
@@ -21,5 +20,35 @@ app.controller('tableController', ['$scope', '$http', function($scope, $http){
     
     $scope.update = function(tableData){
         $scope.editingData[tableData.id] = false
+    }
+    
+    $scope.addSurvey= function(){
+        var survey = {
+            name: $scope.name,
+            description: $scope.description,
+            url: $scope.url,
+            id: ++maxID
+        }    
+        $scope.editingData[maxID] = false
+        $scope.names.push(survey)
+        
+        $scope.name = ''
+        $scope.description = ''
+        $scope.url = ''
+    }
+    
+    $scope.deleteSurvey = function(tableData){
+        var index = -1
+        for (var i = 0; i < $scope.names.length; i++){
+            if ($scope.names[i].id === tableData.id){
+                index = i
+                break
+            }
+        }
+        if (index === -1){
+            alert("Oh no!")
+        }
+        
+        $scope.names.splice(index, 1)
     }
 }])
